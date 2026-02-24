a367f92863f12104b57cfc417cf20edd761195f4
unknown <jamee.islam2018@gmail.com>
2026-02-24 22:35:01 +0600

    Added more functions

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ import utils
 
 result1 = utils.add(10, 5)
 result2 = utils.subtract(10, 5)
+result3 = utils.multiply(10, 5)
+result4 = utils.divide(10, 5)
 
 print("Addition:", result1)
 print("Subtraction:", result2)
+print("Multiplication:", result3)
+print("Division:", result4)
